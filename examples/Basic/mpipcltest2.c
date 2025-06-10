150b0680f7304d39b14530a10ac7c005efc4f7ef
Derek Schafer <schaferderek96@gmail.com>
2025-06-10 09:29:35 -0600

    Add formatting style guide to MPIPCL (#8)
    * Added clang-format and applied to all tests and source code (but not EuroMPI examples)

--- examples/Basic/mpipcltest2.c
+++ examples/Basic/mpipcltest2.c
@@ -1,96 +1,109 @@
 /* Sample program to test the partitioned communication API.
  * This version uses threads on the receive side also.
  *
- * To compile: 
+ * To compile:
  *    mpicc -O -Wall -fopenmp -o mpipcltest2 mpipcltest2.c mpipcl.c
  * To run:
  *    mpirun -np 2 ./mpipcltest2 <npartitions> <bufsize>
  *    NOTE: bufsize % npartitions == 0
  */
-#include <stdio.h>
-#include <stdlib.h>
 #include <assert.h>
 #include <mpi.h>
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "mpipcl.h"
 
-int main(int argc, char *argv[]) {
-  int rank, size, nparts, bufsize, count, tag = 0xbad;
-  int i, j, provided;
-  double *buf, sum;
-  MPIX_Request req;
-  MPI_Status status;
+int main(int argc, char* argv[])
+{
+    int rank, size, nparts, bufsize, count, tag = 0xbad;
+    int i, j, provided;
+    double *buf, sum;
+    MPIX_Request req;
+    MPI_Status status;
 
-  MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &provided);
-  assert(provided == MPI_THREAD_SERIALIZED);
-  
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  assert(size == 2);
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &provided);
+    assert(provided == MPI_THREAD_SERIALIZED);
 
-  if (argc != 3) {
-    printf("Usage: %s <#partitions> <bufsize>\n", argv[0]);
-    MPI_Abort(MPI_COMM_WORLD, -1);
-  }
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    assert(size == 2);
 
-  nparts = atoi(argv[1]);
-  bufsize = atoi(argv[2]);
-  count = bufsize/nparts;
-  
-  if ((size != 2) || (bufsize % nparts != 0)) {
-    printf("comm size must be 2 and bufsize must be divisible by nparts\n");
-    MPI_Abort(MPI_COMM_WORLD, -1);
-  }
-  
-  buf = malloc(sizeof(double) * bufsize);
-
-  if (rank == 0) { /* sender */
-    MPIX_Psend_init(buf, nparts, count, MPI_DOUBLE, 1, tag, MPI_COMM_WORLD, MPI_INFO_NULL, &req);
-    MPIX_Start(&req);
-
-    #pragma omp parallel for private(j) shared(buf,req) num_threads(nparts)
-    for (i = 0; i < nparts; i++) {
-       /* initialize part of buffer in each thread */
-       for (j = 0; j < count; j++) 
-          buf[j + i*count] = j + i*count + 1.0;
-
-       /* indicate buffer is ready */
-       MPIX_Pready(i, &req);
+    if (argc != 3)
+    {
+        printf("Usage: %s <#partitions> <bufsize>\n", argv[0]);
+        MPI_Abort(MPI_COMM_WORLD, -1);
     }
-    MPIX_Wait(&req, &status);
 
-  } else {         /* receiver */
-    MPIX_Precv_init(buf, nparts, count, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, MPI_INFO_NULL, &req);
-    MPIX_Start(&req);
+    nparts  = atoi(argv[1]);
+    bufsize = atoi(argv[2]);
+    count   = bufsize / nparts;
 
-    #pragma omp parallel for shared(buf,req,sum) num_threads(nparts)
-      for (i = 0; i < nparts; i++) {
-        int j, flag = 0; 
-        double mysum = 0.0;
-        while (!flag) {
-            /* check if partition has been received */
-            MPIX_Parrived(&req, i, &flag); 
+    if ((size != 2) || (bufsize % nparts != 0))
+    {
+        printf("comm size must be 2 and bufsize must be divisible by nparts\n");
+        MPI_Abort(MPI_COMM_WORLD, -1);
+    }
 
-            if (flag) {
-              /* compute the partial sum of the values received */
-              for (j = 0, mysum = 0.0; j < count; j++)
-                mysum += buf[j + i*count];
+    buf = malloc(sizeof(double) * bufsize);
 
-              /* update global sum */
-              #pragma omp critical 
-              sum += mysum;
+    if (rank == 0)
+    { /* sender */
+        MPIX_Psend_init(buf, nparts, count, MPI_DOUBLE, 1, tag, MPI_COMM_WORLD,
+                        MPI_INFO_NULL, &req);
+        MPIX_Start(&req);
+
+#pragma omp parallel for private(j) shared(buf, req) num_threads(nparts)
+        for (i = 0; i < nparts; i++)
+        {
+            /* initialize part of buffer in each thread */
+            for (j = 0; j < count; j++)
+                buf[j + i * count] = j + i * count + 1.0;
+
+            /* indicate buffer is ready */
+            MPIX_Pready(i, &req);
+        }
+        MPIX_Wait(&req, &status);
+    }
+    else
+    { /* receiver */
+        MPIX_Precv_init(buf, nparts, count, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD,
+                        MPI_INFO_NULL, &req);
+        MPIX_Start(&req);
+
+#pragma omp parallel for shared(buf, req, sum) num_threads(nparts)
+        for (i = 0; i < nparts; i++)
+        {
+            int j, flag = 0;
+            double mysum = 0.0;
+            while (!flag)
+            {
+                /* check if partition has been received */
+                MPIX_Parrived(&req, i, &flag);
+
+                if (flag)
+                {
+                    /* compute the partial sum of the values received */
+                    for (j = 0, mysum = 0.0; j < count; j++)
+                        mysum += buf[j + i * count];
+
+/* update global sum */
+#pragma omp critical
+                    sum += mysum;
+                }
             }
         }
-      }
 
-    MPIX_Wait(&req, MPI_STATUS_IGNORE);
-    printf("#partitions = %d bufsize = %d count = %d sum = %f (%f)\n", 
-           nparts, bufsize, count, sum, ((double)bufsize*(bufsize+1))/2.0);
-  }
+        MPIX_Wait(&req, MPI_STATUS_IGNORE);
+        printf("#partitions = %d bufsize = %d count = %d sum = %f (%f)\n",
+               nparts, bufsize, count, sum,
+               ((double)bufsize * (bufsize + 1)) / 2.0);
+    }
 
-  MPIX_Request_free(&req);
-  free(buf);
-  MPI_Finalize();
+    MPIX_Request_free(&req);
+    free(buf);
+    MPI_Finalize();
 
-  return 0;
+    return 0;
 }
--- src/pt2pt/send.c
+++ src/pt2pt/send.c
@@ -1,108 +1,121 @@
-#include "mpipcl.h"
-
 #include <math.h>
+
+#include "mpipcl.h"
 
 // call send function on all marked partitions
 // catchup function for sync thread.
-void send_ready(MPIPCL_REQUEST *request)
+void send_ready(MPIPCL_REQUEST* request)
 {
-	// call send on each currently marked partition
-	for (int i = 0; i < request->local_parts; i++)
-	{
-		if (request->local_status[i])
-		{
-			general_send(i, request);
-		}
-	}
+    // call send on each currently marked partition
+    for (int i = 0; i < request->local_parts; i++)
+    {
+        if (request->local_status[i])
+        {
+            general_send(i, request);
+        }
+    }
 }
 
-static inline void map_local_to_network_partitions(int user_partition_id, MPIPCL_REQUEST* request, int* start, int* end)
+static inline void map_local_to_network_partitions(int user_partition_id,
+                                                   MPIPCL_REQUEST* request,
+                                                   int* start, int* end)
 {
-	int user_partitions = request->local_parts;
-	int network_partitions = request->parts;
+    int user_partitions    = request->local_parts;
+    int network_partitions = request->parts;
 
-	int temp_start = (network_partitions / user_partitions) * user_partition_id;
-	int temp_end   = ceil(network_partitions / user_partitions) * (user_partition_id + 1);
+    int temp_start = (network_partitions / user_partitions) * user_partition_id;
+    int temp_end =
+        ceil(network_partitions / user_partitions) * (user_partition_id + 1);
 
-	MPIPCL_DEBUG("Partition %d - Start %d End %d \n", user_partition_id, temp_start, temp_end);
-	assert(temp_start >= 0 && temp_end > temp_start && temp_end <= network_partitions);
+    MPIPCL_DEBUG("Partition %d - Start %d End %d \n", user_partition_id,
+                 temp_start, temp_end);
+    assert(temp_start >= 0 && temp_end > temp_start &&
+           temp_end <= network_partitions);
 
-	*start = temp_start;
-	*end   = temp_end;
+    *start = temp_start;
+    *end   = temp_end;
 }
 
 // given partition id, send now ready internal requests.
-void general_send(int id, MPIPCL_REQUEST *request)
+void general_send(int id, MPIPCL_REQUEST* request)
 {
-	int start_part = 0;
-	int end_part   = 0;
-	int threshold  = 0;
-	MPIPCL_DEBUG("User Partitions %d - Network Partitions %d\n", request->local_parts, request->parts);
-	if(request->local_parts <= request->parts)
-	{
-		map_local_to_network_partitions(id, request, &start_part, &end_part);
-		threshold = 1;
-	}
-	else /* Must be: if(request->local_parts > request->parts) */
-	{
-		threshold = request->local_parts / request->parts;
-		start_part = id / threshold;
-		end_part   = start_part+1;
-	}
+    int start_part = 0;
+    int end_part   = 0;
+    int threshold  = 0;
+    MPIPCL_DEBUG("User Partitions %d - Network Partitions %d\n",
+                 request->local_parts, request->parts);
+    if (request->local_parts <= request->parts)
+    {
+        map_local_to_network_partitions(id, request, &start_part, &end_part);
+        threshold = 1;
+    }
+    else /* Must be: if(request->local_parts > request->parts) */
+    {
+        threshold  = request->local_parts / request->parts;
+        start_part = id / threshold;
+        end_part   = start_part + 1;
+    }
 
-	MPIPCL_DEBUG("User Partition %d - Start %d End %d \n", id, start_part, end_part);
+    MPIPCL_DEBUG("User Partition %d - Start %d End %d \n", id, start_part,
+                 end_part);
 
-	// for each internal request affected
-	for (int i = start_part; i < end_part; i++)
-	{
-		// increase the number of "local" partitions ready (using atomics)
-		int prior_value = atomic_fetch_add_explicit(&request->internal_status[i], 1, memory_order_relaxed);
+    // for each internal request affected
+    for (int i = start_part; i < end_part; i++)
+    {
+        // increase the number of "local" partitions ready (using atomics)
+        int prior_value = atomic_fetch_add_explicit(
+            &request->internal_status[i], 1, memory_order_relaxed);
 
-		MPIPCL_DEBUG("Network Partition %d - Count: %d, Threshold: %d\n", i, prior_value+1, threshold);
+        MPIPCL_DEBUG("Network Partition %d - Count: %d, Threshold: %d\n", i,
+                     prior_value + 1, threshold);
 
-		// if the number of local partitions needed for one network partition are ready
-		if (prior_value + 1 == threshold)
-		{
-			// start associated request
-			MPIPCL_DEBUG("Starting request %d %p \n", i, (void *) (&request->request[i]));
-			int ret_val = MPI_Start(&request->request[i]);
-			assert(MPI_SUCCESS == ret_val);
-		}
-	}
+        // if the number of local partitions needed for one network partition
+        // are ready
+        if (prior_value + 1 == threshold)
+        {
+            // start associated request
+            MPIPCL_DEBUG("Starting request %d %p \n", i,
+                         (void*)(&request->request[i]));
+            int ret_val = MPI_Start(&request->request[i]);
+            assert(MPI_SUCCESS == ret_val);
+        }
+    }
 }
 
 // maps recv_buffer offset -- parried related.
 // returns 1 if ready, 0 otherwise.
 // all results from race condition identical, no lock needed.
-int map_recv_buffer(int id, MPIPCL_REQUEST *request)
+int map_recv_buffer(int id, MPIPCL_REQUEST* request)
 {
-	if (request->local_status[id] == true)
-	{
-		return 1;
-	}
+    if (request->local_status[id] == true)
+    {
+        return 1;
+    }
 
-	int start = 0;
-	int end = 0;
-	if(request->local_parts <= request->parts)
-	{
-		map_local_to_network_partitions(id, request, &start, &end);
-	}
-	else
-	{
-		int threshold = request->local_parts / request->parts;
-		start = id / threshold;
-		end   = start+1;
-	}
+    int start = 0;
+    int end   = 0;
+    if (request->local_parts <= request->parts)
+    {
+        map_local_to_network_partitions(id, request, &start, &end);
+    }
+    else
+    {
+        int threshold = request->local_parts / request->parts;
+        start         = id / threshold;
+        end           = start + 1;
+    }
 
-	// check status of dependent requests
-	int flag = 0;
-	MPIPCL_DEBUG("User Partitions %d - Network Partitions %d\n", request->local_parts, request->parts);
-	MPIPCL_DEBUG("Checking Requests: [%d: %d)\n", start, end);
-	int ret_val = MPI_Testall(end - start, &request->request[start], &flag, MPI_STATUSES_IGNORE);
-	assert(MPI_SUCCESS == ret_val);
+    // check status of dependent requests
+    int flag = 0;
+    MPIPCL_DEBUG("User Partitions %d - Network Partitions %d\n",
+                 request->local_parts, request->parts);
+    MPIPCL_DEBUG("Checking Requests: [%d: %d)\n", start, end);
+    int ret_val = MPI_Testall(end - start, &request->request[start], &flag,
+                              MPI_STATUSES_IGNORE);
+    assert(MPI_SUCCESS == ret_val);
 
-	// if true store for future shortcut
-	request->local_status[id] = flag;
+    // if true store for future shortcut
+    request->local_status[id] = flag;
 
-	return flag;
+    return flag;
 }
