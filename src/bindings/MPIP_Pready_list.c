3a2d38a301c5d9681319148fdef29f8cd490e6b5
aworley16 <apworley@gmail.com>
2025-09-29 11:17:30 -0500

    testing to see if fancy included needed for building package

--- src/bindings/MPIP_Pready_list.c
+++ src/bindings/MPIP_Pready_list.c
@@ -8,11 +8,11 @@
 extern "C" {
 #endif
 
-int MPIP_Pready_range(int partition_low, int partition_high, MPIP_Request* request)
+int MPIP_Pready_list(int length, int array_of_partitions[], MPIP_Request* request)
 {
-    for (int i = partition_low; i <= partition_high; i++)
+    for (int i = 0; i < length; i++)
     {
-        int ret_val = MPIP_Pready(i, request);
+        int ret_val = MPIP_Pready(array_of_partitions[i], request);
         assert(MPI_SUCCESS == ret_val);
     }
     return MPI_SUCCESS;
